a678c81a2b4fcf4b7bd2fc8fe62efa780bb73844
sivamotamarri <sivakrishna.motamarri@gmail.com>
2013-04-23 11:03:53 +0530

    added routes for home page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wedding::Application.config.secret_token = '58173c937ddd5653547bb4d6489e7c02459456d44b807d1733020282a1fb74c4a86aa8a3a6e60e4c8a520c69d0de11348fba183d46f8480e80d9c296a4605283'
