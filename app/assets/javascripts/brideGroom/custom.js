d708843e5da9509aac078a30fa53a7525387cb53
sivamotamarri <sivakrishna.motamarri@gmail.com>
2013-04-24 12:54:12 +0530

    added marquee

--- app/assets/javascripts/brideGroom/custom.js
+++ app/assets/javascripts/brideGroom/custom.js
@@ -3,16 +3,41 @@ $(document).ready(function(){
 
 		$("#title01").css("display","block");
 		$("#title01").lettering().animateLetters({opacity:0},null,{randomOrder:false,time:1000,reset:true});
-		
+
 		delayLettringFn("dlName", 2000);
 		delayLettringFn("ddName", 3000);
-		
+
 		delayLettringFn("dlQual", 5000);
 		delayLettringFn("ddQual", 7000);
-		
+
 		delayLettringFn("dlAge", 8500);
 		delayLettringFn("ddAge", 9500);
-		
+
+
+		var BigDay = new Date("3 May 2013, 11:30:00");
+	var msPerDay = 24 * 60 * 60 * 1000 ;
+
+
+	window.setInterval(function(){
+		var today = new Date();
+		var timeLeft = (BigDay.getTime() - today.getTime());
+
+		var e_daysLeft = timeLeft / msPerDay;
+		var daysLeft = Math.floor(e_daysLeft);
+
+		var e_hrsLeft = (e_daysLeft - daysLeft)*24;
+		var hrsLeft = Math.floor(e_hrsLeft);
+
+		var e_minsLeft = (e_hrsLeft - hrsLeft)*60;
+		var minsLeft = Math.floor(e_minsLeft);
+
+		var e_secsLeft = (e_minsLeft - minsLeft)*60;
+		var secsLeft = Math.floor(e_secsLeft);
+
+
+		var timeString = daysLeft + ' : ' + hrsLeft + ' : ' + minsLeft + ' : ' + secsLeft;
+		$('#countdown').html(timeString);
+	}, 1000);
 });
 function delayLettringFn(fldId, delayTime){
 	setTimeout(function() {
@@ -30,11 +55,11 @@ var headerHeight = 300;		// How tall the header is.
 var restartPosition = -(imageHeight - headerHeight);
 function scrollBg(){
 	//Go to next pixel row.
-	current -= step;	
+	current -= step;
 	//If at the end of the image, then go to the top.
 	if (current == restartPosition){
 		current = 0;
-	}	
+	}
 	//Set the CSS of the header.
 	$('#header').css("background-position","0 "+current+"px");
 }
